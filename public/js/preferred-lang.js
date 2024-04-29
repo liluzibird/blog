df6fe3ceecd641c9034e2742f47d1519054aa130
liluzibird <57073805+liluzibird@users.noreply.github.com>
2024-04-28 17:25:49 -0700

    post redirect blog

--- public/js/preferred-lang.js
+++ public/js/preferred-lang.js
@@ -1,6 +1,6 @@
 (() => {
   // ns-params:@params
-  var params_default = { fallback: "http://localhost:40219/", homes: { en: "http://localhost:40219/" } };
+  var params_default = { fallback: "http://localhost:46799/", homes: { en: "http://localhost:46799/" } };
 
   // <stdin>
   (() => {
